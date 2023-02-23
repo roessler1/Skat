88a9e9bb5b0f645b2b1fd6dc7a21efd3bda34732
Maximlegreat <Maximiliant.mueller@web.de>
2023-02-23 18:32:36 +0100

    Added game-view.fxml and GameEvents.java.

--- Gui/src/client/gui/pane_events/GameEvents.java
+++ Gui/src/client/gui/pane_events/GameEvents.java
@@ -1,18 +1,37 @@
 package client.gui.pane_events;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
-public class GameEvents {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameEvents implements Initializable {
 
     @FXML
     ToggleGroup gameId;
 
     @FXML
+    ToggleGroup priceStaging;
+
+    @FXML
+    RadioButton normal;
+    @FXML
     protected void onConfirmButtonClicked() {
         RadioButton selectedRadioButton = (RadioButton) gameId.getSelectedToggle();
         byte game = Byte.parseByte(selectedRadioButton.getId());
         // TODO -> Confirm Button
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if(0 == 0/* TODO -> Handspiel? */) {
+            normal.setText("Handspiel");
+        } else {
+            normal.setText("Normales Spiel");
+        }
     }
 }
