c37f39779a1b5580c5cabeb5b44bb3aced91e098
Maximlegreat <Maximiliant.mueller@web.de>
2023-02-23 19:36:06 +0100

    Added results-view.fxml and ResultsEvents.java.

---
+++ Gui/src/client/gui/pane_events/ResultsEvents.java
@@ -0,0 +1,42 @@
+package client.gui.pane_events;
+
+import client.gui.pane_controller.GuiController;
+import client.logic.LogicEvents;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ResultsEvents implements Initializable {
+
+    @FXML
+    Text player1;
+    @FXML
+    Text player1Points;
+    @FXML
+    Text player2;
+    @FXML
+    Text player2Points;
+    @FXML
+    Text player3;
+    @FXML
+    Text player3Points;
+
+    @FXML
+    protected void onNextRoundButtonClicked() {
+        // TODO -> Nächste Runde Button
+    }
+
+    @FXML
+    protected void onBackButtonClicked() {
+        LogicEvents.deleteInstance();
+        GuiController.getInstance().loadMainMenu();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // TODO -> Spieler Namen
+    }
+}
