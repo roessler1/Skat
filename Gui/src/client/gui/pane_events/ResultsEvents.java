82f7c85baca2086d95bda55909e1300ba5dd4865
roessle1 <basti.roessler@t-online.de>
2023-02-28 20:52:07 +0100

    fixed result bugs

--- Gui/src/client/gui/pane_events/ResultsEvents.java
+++ Gui/src/client/gui/pane_events/ResultsEvents.java
@@ -4,6 +4,7 @@ import client.gui.pane_controller.GuiController;
 import client.logic.LogicEvents;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -14,15 +15,15 @@ public class ResultsEvents implements Initializable {
     @FXML
     Text player1;
     @FXML
-    Text player1Points;
+    Label player1Points;
     @FXML
     Text player2;
     @FXML
-    Text player2Points;
+    Label player2Points;
     @FXML
     Text player3;
     @FXML
-    Text player3Points;
+    Label player3Points;
 
     @FXML
     protected void onNextRoundButtonClicked() {
