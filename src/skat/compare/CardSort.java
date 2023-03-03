e7cd21355023520f89ed1b41a457715f0f08341c
roessle1 <basti.roessler@t-online.de>
2023-03-03 17:29:48 +0100

    fixed bug connection between different colors

--- src/skat/compare/CardSort.java
+++ src/skat/compare/CardSort.java
@@ -19,7 +19,7 @@ public class CardSort {
         return cards;
     }
 
-    public byte compareByGame(Card card1, Card card2, byte game) {
+    private byte compareByGame(Card card1, Card card2, byte game) {
         switch(game) {
             case 9 -> {
                 return compareDiamonds(card1, card2);
@@ -47,57 +47,152 @@ public class CardSort {
 
     private byte compareDiamonds(Card card1, Card card2) {
         byte result = compareGrand(card1, card2);
-        if(card2.getColor() == 9 && card1.getValue() != 2) {
-            if(card1.getColor() > 9)
-                result = 1;
-        }
-        if(card1.getColor() == 9 && card2.getValue() != 2) {
-            if(card2.getColor() > 9)
-                result = -1;
-        }
-        return result;
+        return checkDiamonds(card1, card2, result);
     }
 
     private byte compareHearts(Card card1, Card card2) {
         byte result = compareGrand(card1, card2);
-        if(card2.getColor() == 10 && card1.getValue() != 2) {
-            if(card1.getColor() > 10)
-                result = 1;
-        }
-        if(card1.getColor() == 10 && card2.getValue() != 2) {
-            if(card2.getColor() > 10)
-                result = -1;
-        }
-        return result;
+        return checkHearts(card1, card2, result);
     }
 
     private byte compareSpades(Card card1, Card card2) {
         byte result = compareGrand(card1, card2);
-        if(card2.getColor() == 11 && card1.getValue() != 2) {
-            if(card1.getColor() > 11)
-                result = 1;
-        }
-        if(card1.getColor() == 11 && card2.getValue() != 2) {
-            if(card2.getColor() > 11)
-                result = -1;
-        }
-        return result;
+        return checkSpades(card1, card2, result);
     }
 
     private byte compareClubs(Card card1, Card card2) {
         byte result = compareGrand(card1, card2);
-        if(card2.getColor() == 12 && card1.getValue() != 2) {
-            if(card1.getColor() > 12)
-                result = 1;
-        }
-        if(card1.getColor() == 12 && card2.getValue() != 2) {
-            if(card2.getColor() > 12)
+        return checkClubs(card1, card2, result);
+    }
+
+    private byte compareGrand(Card card1, Card card2) {
+        byte result = compareGrandTrick(card1, card2);
+        if(result == 0) {
+            if(card1.getColor() > card2.getColor())
                 result = -1;
+            if(card1.getColor() < card2.getColor())
+                result = 1;
         }
         return result;
     }
 
-    private byte compareGrand(Card card1, Card card2) {
+    private byte compareNull(Card card1, Card card2) {
+        if(card1.getId() > card2.getId()) {
+            return 1;
+        }
+        if(card1.getId() < card2.getId()) {
+            return -1;
+        }
+        return 0;
+    }
+
+    private byte compareSameColor(Card card1, Card card2) {
+        if(card1.getValue() > card2.getValue())
+            return -1;
+        if(card1.getValue() < card2.getValue())
+            return 1;
+        if(card1.getId() > card2.getId())
+            return 1;
+        if(card1.getId() < card2.getId())
+            return -1;
+        return 0;
+    }
+
+    private byte checkClubs(Card card1, Card card2, byte result) {
+        if(card2.getColor() == 12 && card1.getValue() != 2) {
+            if(card1.getColor() != 12)
+                return 1;
+        }
+        if(card1.getColor() == 12 && card2.getValue() != 2) {
+            if(card2.getColor() != 12)
+                return -1;
+        }
+        return result;
+    }
+
+    private byte checkSpades(Card card1, Card card2, byte result) {
+        if(card2.getColor() == 11 && card1.getValue() != 2) {
+            if(card1.getColor() != 11)
+                return 1;
+        }
+        if(card1.getColor() == 11 && card2.getValue() != 2) {
+            if(card2.getColor() != 11)
+                return -1;
+        }
+        return result;
+    }
+
+    private byte checkHearts(Card card1, Card card2, byte result) {
+        if(card2.getColor() == 10 && card1.getValue() != 2) {
+            if(card1.getColor() != 10)
+                return 1;
+        }
+        if(card1.getColor() == 10 && card2.getValue() != 2) {
+            if(card2.getColor() != 10)
+                return -1;
+        }
+        return result;
+    }
+
+    private byte checkDiamonds(Card card1, Card card2, byte result) {
+        if(card2.getColor() == 9 && card1.getValue() != 2) {
+            if(card1.getColor() != 9)
+                return 1;
+        }
+        if(card1.getColor() == 9 && card2.getValue() != 2) {
+            if(card2.getColor() != 9)
+                return -1;
+        }
+        return result;
+    }
+
+    public byte compareForTrick(Card card1, Card card2, byte gameId) {
+        switch(gameId) {
+            case 9 -> {
+                return compareDiamondsTrick(card1, card2);
+            }
+            case 10 -> {
+                return compareHeartsTrick(card1, card2);
+            }
+            case 11 -> {
+                return compareSpadesTrick(card1, card2);
+            }
+            case 12 -> {
+                return compareClubsTrick(card1, card2);
+            }
+            case 24 -> {
+                return compareGrandTrick(card1, card2);
+            }
+            case 23, 35, 46, 59 -> {
+                return compareNullTrick(card1, card2);
+            }
+            default -> {
+                return 0;
+            }
+        }
+    }
+
+    private byte compareDiamondsTrick(Card card1, Card card2) {
+        byte result = compareGrandTrick(card1, card2);
+        return checkDiamonds(card1, card2, result);
+    }
+
+    private byte compareHeartsTrick(Card card1, Card card2) {
+        byte result = compareGrandTrick(card1, card2);
+        return checkHearts(card1, card2, result);
+    }
+
+    private byte compareSpadesTrick(Card card1, Card card2) {
+        byte result = compareGrandTrick(card1, card2);
+        return checkSpades(card1, card2, result);
+    }
+
+    private byte compareClubsTrick(Card card1, Card card2) {
+        byte result = compareGrandTrick(card1, card2);
+        return checkClubs(card1, card2, result);
+    }
+
+    private byte compareGrandTrick(Card card1, Card card2) {
         if(card1.getValue() == 2 && card2.getValue() == 2) {
             if(card1.getColor() > card2.getColor())
                 return -1;
@@ -109,28 +204,17 @@ public class CardSort {
         if(card2.getValue() == 2)
             return 1;
         if(card1.getColor() == card2.getColor()) {
-            if(card1.getValue() > card2.getValue())
-                return -1;
-            if(card1.getValue() < card2.getValue())
-                return 1;
+            return compareSameColor(card1, card2);
+        }
+        return 0;
+    }
+
+    private byte compareNullTrick(Card card1, Card card2) {
+        if(card1.getColor() == card2.getColor()) {
             if(card1.getId() > card2.getId())
                 return 1;
             if(card1.getId() < card2.getId())
                 return -1;
-        }
-        if(card1.getColor() > card2.getColor())
-            return -1;
-        if(card1.getColor() < card2.getColor())
-            return 1;
-        return 0;
-    }
-
-    private byte compareNull(Card card1, Card card2) {
-        if(card1.getId() > card2.getId()) {
-            return 1;
-        }
-        if(card1.getId() < card2.getId()) {
-            return -1;
         }
         return 0;
     }
